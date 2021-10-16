3fbbbf58874de9ad4b6fd2de04c10e336c1c9701
Fikadu <fikadufilipos@Fikadus-MacBook-Pro.local>
2021-10-16 13:09:07 -0500

    application initialized

---
+++ src/main/java/com/fikadu/stripeintegrationspringboot/StripeIntegrationSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.fikadu.stripeintegrationspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StripeIntegrationSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StripeIntegrationSpringbootApplication.class, args);
+    }
+
+}
